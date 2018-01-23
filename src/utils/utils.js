bb99c7e8fe9b65d251468072d7caa66c3b7e1b81
Christian <ChrisWcs@users.noreply.github.com>
2018-01-23 13:23:47 -0700

    Create function in utils that converts binary to baseten

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,19 @@
-const convertToBinary = () => {
+export const convertToBinary = () => {
 
+
+    
 };
 
-const convertToBaseten = () => {
+export const convertToBaseten = (value) => {
+    let arrOfNums = value.split("").map( x => parseInt(x));
+    let total = 0;
+
+    let bitIndex = 1;
+
+    for(let i = (arrOfNums.length - 1); i >= 0; i--){
+        total += (arrOfNums[i] * bitIndex);
+        bitIndex = bitIndex * 2;
+    }
     
+    return total;
 }
